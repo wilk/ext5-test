8d4b61d4e8cde78a7318ce8a4f85e15db759add7
Vincenzo (Wilk) Ferrari <wilk3ert@gmail.com>
2014-06-11 11:05:28 +0200

    removed old ext5 beta boilerplate

--- app/controller/Main.js
+++
@@ -1,3 +0,0 @@
-Ext.define('ET.controller.Main', {
-    extend: 'Ext.app.Controller'
-});
--- app/controller/Root.js
+++
@@ -1,6 +0,0 @@
-/**
- * The main application controller. This is a good place to handle things like routes.
- */
-Ext.define('ET.controller.Root', {
-    extend: 'Ext.app.Controller'
-});
